3c33729f8d6ad489529c1f93bc893bf8c536ae52
meta <donahiyo@outlook.jp>
2025-03-11 19:38:26 +0900

    4

--- src/main/java/com/meta/serveritemsmod/entity/CustomCorpseRenderer.java
+++ src/main/java/com/meta/serveritemsmod/entity/CustomCorpseRenderer.java
@@ -1,28 +1,39 @@
 package com.meta.serveritemsmod.entity;
 
+import com.meta.serveritemsmod.entity.CustomCorpseEntity;
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.block.BlockRenderDispatcher;
 import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.core.BlockPos;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.phys.AABB;
+import net.minecraft.world.level.block.Blocks;
 
-public class CustomCorpseRenderer extends EntityRenderer<CustomCorpseEntity>{
+public class CustomCorpseRenderer extends EntityRenderer<CustomCorpseEntity> {
 
-    private static final ResourceLocation TEXTURE = new ResourceLocation("serveritemsmod", "textures/entity/custom_corpse.png");
+    private final BlockRenderDispatcher blockRenderer;
 
     public CustomCorpseRenderer(EntityRendererProvider.Context context) {
         super(context);
-}
+        this.blockRenderer = context.getBlockRenderDispatcher();
+    }
+
+    @Override
+    public void render(CustomCorpseEntity entity, float yaw, float partialTicks, PoseStack poseStack, MultiBufferSource buffer, int packedLight) {
+        poseStack.pushPose();
+
+        // エンティティの位置を調整
+        poseStack.translate(-0.5, 0.0, -0.5);
+
+        // ブロックを描画（ここでは `Blocks.CHEST.defaultBlockState()` を指定）
+        blockRenderer.renderSingleBlock(Blocks.CHEST.defaultBlockState(), poseStack, buffer, packedLight, 0);
+
+        poseStack.popPose();
+    }
 
     @Override
     public ResourceLocation getTextureLocation(CustomCorpseEntity entity) {
-        return TEXTURE;
+        return null; // ブロック描画なのでテクスチャ不要
     }
-
-    @Override
-    public void render(CustomCorpseEntity entity, float entityYaw, float partialTicks, PoseStack poseStack, MultiBufferSource buffer, int packedLight) {
-        super.render(entity, entityYaw, partialTicks, poseStack, buffer, packedLight);
-    }
-
 }
--- src/main/java/com/meta/serveritemsmod/gui/client/CustomCorpseScreen.java
+++ src/main/java/com/meta/serveritemsmod/gui/client/CustomCorpseScreen.java
@@ -8,12 +8,12 @@ import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.resources.ResourceLocation;
 
 public class CustomCorpseScreen extends AbstractContainerScreen<CustomCorpseMenu> {
-    private static final ResourceLocation TEXTURE = new ResourceLocation("serveritemsmod", "textures/gui/custom_corpse.png");
+    private static final ResourceLocation TEXTURE = new ResourceLocation("minecraft", "textures/gui/container/generic_54.png");
 
     public CustomCorpseScreen(CustomCorpseMenu menu, Inventory playerInventory, Component title) {
         super(menu, playerInventory, title);
         this.imageWidth = 176;
-        this.imageHeight = 166;
+        this.imageHeight = 250;
     }
 
     @Override
@@ -26,6 +26,15 @@ public class CustomCorpseScreen extends AbstractContainerScreen<CustomCorpseMenu
         this.renderBackground(guiGraphics);
         super.render(guiGraphics, mouseX, mouseY, delta);
         this.renderTooltip(guiGraphics, mouseX, mouseY);
+    }
+
+    @Override
+    protected void renderLabels(GuiGraphics guiGraphics, int mouseX, int mouseY) {
+        // **タイトルの位置を変更 (X: 8, Y: 6)**
+        guiGraphics.drawString(this.font, this.title, 8, 6, 0x404040, false);
+
+        // **プレイヤーのインベントリラベルの位置を変更 (X: 8, Y: 130)**
+        guiGraphics.drawString(this.font, this.playerInventoryTitle, 8, 130, 0x404040, false);
     }
 }
 
